3839cd5f5be13c698f5b2a225189bd0adcff1a84
buvanesswaraneb <buvaneswaran.e.b@gmail.com>
2026-09-21 22:50:48 +0530

    bug fix : Collision is not working with overlaping sprites

--- src/main/java/main/CollisonEngine.java
+++ src/main/java/main/CollisonEngine.java
@@ -8,6 +8,19 @@ public class CollisonEngine {
         this.gp = gp;
     }
 
+
+    private boolean isSolid(int row , int col){
+
+        // map
+        int side1 =  gp.tileManager.map[row][col];
+        boolean ismap = gp.tileManager.tiles[side1].collision;
+        // map 2
+        int side2 = gp.tileManager.overlay_map[row][col];
+        boolean isOverlay = gp.tileManager.tiles[side2].collision;
+
+        return ismap || isOverlay;
+    }
+
     public void Check_tile(Entity entity){
 
         // solid rectangle world coordinates
@@ -22,41 +35,31 @@ public class CollisonEngine {
         int entity_top_row = top_side_world_y / gp.tile_size;
         int entity_bottom_row = bottom_side_world_y / gp.tile_size;
 
-        int side1, side2;
-
         switch (entity.directions){
             case "up":{
                 entity_top_row = (top_side_world_y - entity.speed) / gp.tile_size;
-                side1 = gp.tileManager.map[entity_top_row][entity_right_col];
-                side2 = gp.tileManager.map[entity_top_row][entity_left_col];
-                if(gp.tileManager.tiles[side1].collision || gp.tileManager.tiles[side2].collision) {
+                if(isSolid(entity_top_row,entity_right_col) || isSolid(entity_top_row,entity_left_col)){
                     entity.collisionOn = true;
                 }
                 break;
             }
             case "down":{
                 entity_bottom_row = (bottom_side_world_y + entity.speed) / gp.tile_size;
-                side1 = gp.tileManager.map[entity_bottom_row][entity_left_col];
-                side2 = gp.tileManager.map[entity_bottom_row][entity_right_col];
-                if(gp.tileManager.tiles[side1].collision || gp.tileManager.tiles[side2].collision) {
+                if(isSolid(entity_bottom_row,entity_left_col) || isSolid(entity_bottom_row,entity_right_col)){
                     entity.collisionOn = true;
                 }
                 break;
             }
             case "left":{
                 entity_left_col = (left_side_world_x - entity.speed) / gp.tile_size;
-                side1 = gp.tileManager.map[entity_top_row][entity_left_col];
-                side2 = gp.tileManager.map[entity_bottom_row][entity_left_col];
-                if(gp.tileManager.tiles[side1].collision || gp.tileManager.tiles[side2].collision) {
+                if(isSolid(entity_top_row,entity_left_col)|| isSolid(entity_bottom_row,entity_left_col)){
                     entity.collisionOn = true;
                 }
                 break;
             }
             case "right":{
                 entity_right_col = (right_side_world_x + entity.speed) / gp.tile_size;
-                side1 = gp.tileManager.map[entity_top_row][entity_right_col];
-                side2 = gp.tileManager.map[entity_bottom_row][entity_right_col];
-                if(gp.tileManager.tiles[side1].collision || gp.tileManager.tiles[side2].collision) {
+                if(isSolid(entity_top_row,entity_right_col) || isSolid(entity_bottom_row,entity_right_col)){
                     entity.collisionOn = true;
                 }
                 break;
